6033e6ab1edb7e96325d49a284bbc967e1d2a77d
slyjewbro <login47rus@gmail.com>
2025-10-05 13:08:11 +0500

    Добавил секцию проектов

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request ,redirect
 
 # Создаем объект приложения
 app = Flask(__name__)
@@ -13,7 +13,13 @@ my_data = {
         "email": "login47rus@gmail.com",
         "github": "https://github.com/slyjewbro",
         "telegram": "https://t.me/slyjewbro",
-        "vk": "https://vk.com/slyjewbro1337"
+        "vk": "https://vk.com/slyjewbro1337"},
+    "projects": {
+        "name": "Мой сайт-визитка",
+        "description": "Персональный сайт на Flask с адаптивным дизайном",
+        "technologies": ["Python", "Flask", "HTML", "CSS"],
+        "github_url": "https://github.com/slyjewbro/my-portfolio",
+        "live_url": "https://slyjewbro.pythonanywhere.com"
     }
 }
 
@@ -21,6 +27,14 @@ my_data = {
 @app.route("/")
 def index():
     return render_template("index.html", data=my_data)
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    # Здесь можно добавить отправку на email
+    flash('Сообщение отправлено!', 'success')
+    return redirect('/')
 
 # Запускаем сервер
 if __name__ == "__main__":
